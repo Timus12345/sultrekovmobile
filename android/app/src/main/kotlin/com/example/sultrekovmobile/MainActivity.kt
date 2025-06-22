e06542535dc280f3f19ba7feec3c5d961fdee41a
Timus12345 <reviobandela51451@gmail.com>
2025-06-22 11:35:33 +0700

    1.0

---
+++ android/app/src/main/kotlin/com/example/sultrekovmobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.sultrekovmobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+void main() async {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+
+
+    },
+  ));
+}
